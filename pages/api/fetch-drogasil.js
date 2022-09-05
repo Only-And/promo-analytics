2ee3b9cf7de9ab1be44c85bad646a890209f82b7
Only-And <79213441+Only-And@users.noreply.github.com>
2022-09-05 20:46:43 -0300

    Update fetch-drogasil.js

--- pages/api/fetch-drogasil.js
+++ pages/api/fetch-drogasil.js
@@ -1,7 +1,7 @@
 export default async function getDados (request, response) {
     console.log(request.query.produto)
     base_URL = window.location.hostname
-    const dados = await fetch(`https://${base_URL}/api/req?produto=` + request.query.produto)
+    const dados = await fetch(`https://promo-analitycs.vercel.app/api/req?produto=` + request.query.produto)
     
     const dadosJson = await dados.json();
     response.setHeader('Cache-Control', 's-max-age=86400, stale-while-revalidate')
