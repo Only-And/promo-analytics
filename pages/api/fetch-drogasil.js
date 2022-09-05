97f790b0aac0736ff28cb3d72c295186703a2c25
Only-And <79213441+Only-And@users.noreply.github.com>
2022-09-05 18:08:30 -0300

    Update fetch-drogasil.js

--- pages/api/fetch-drogasil.js
+++ pages/api/fetch-drogasil.js
@@ -1,6 +1,7 @@
 export default async function getDados (request, response) {
     console.log(request.query.produto)
-    const dados = await fetch(`http://localhost:3001/api/req?produto=` + request.query.produto)
+    base_URL = window.location.hostname
+    const dados = await fetch(`https://${base_URL}/api/req?produto=` + request.query.produto)
     
     const dadosJson = await dados.json();
     response.setHeader('Cache-Control', 's-max-age=86400, stale-while-revalidate')
